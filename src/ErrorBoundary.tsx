2d1588d3f9d3841e9b10fb0112cd4efb1406f9d3
y-mattsuda <m.youichi1225@icloud.com>
2022-12-27 15:04:11 +0900

    fix: add type for styled-componets

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,59 +1,65 @@
-import * as React from 'react'
-import styled from 'styled-components'
-import StartButton from './StartButton';
+import * as React from "react";
+import styled from "styled-components";
+import StartButton from "./StartButton";
 
 type ErrorProps = {
-    setIsPlaying: (isPlaying: boolean) => void
-}
+  setIsPlaying: (isPlaying: boolean) => void;
+  children: React.ReactNode;
+};
+
 type ErrorState = {
-    errorMessage: null | string
-}
+  errorMessage: null | string;
+};
 
-export default class ErrorBoundary extends React.Component<ErrorProps, ErrorState> {
-    state: ErrorState = {
-        errorMessage: null
+export default class ErrorBoundary extends React.Component<
+  ErrorProps,
+  ErrorState
+> {
+  state: ErrorState = {
+    errorMessage: null,
+  };
+
+  backToStart = () => {
+    this.props.setIsPlaying(false);
+  };
+
+  render() {
+    const { errorMessage } = this.state;
+    if (errorMessage) {
+      // Show Error Message:
+      return (
+        <Container>
+          <Error>An unexpected error occured: {errorMessage}</Error>
+          <StartButton onClick={this.backToStart}>Back to Start</StartButton>
+        </Container>
+      );
     }
 
-    backToStart = () => {
-        this.props.setIsPlaying(false)
-    }
-
-    render() {
-        const { errorMessage } = this.state
-        if (errorMessage) {
-            // Show Error Message:
-            return (
-                <Container>
-                    <Error>An unexpected error occured: {errorMessage}</Error>
-                    <StartButton onClick={this.backToStart}>Back to Start</StartButton>
-                </Container>
-            )
-        }
-
-        // Otherwise show children:
-        return this.props.children; 
-    }
-    static getDerivedStateFromError(error: Error) {
-        // Update state so the next render will show the fallback UI.
-        return { errorMessage: error.message };
-    }
+    // Otherwise show children:
+    return this.props.children;
+  }
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { errorMessage: error.message };
+  }
 }
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin: auto;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+`;
 const Error = styled.div`
-    color: red;
-    font-size: 1.4em;
-    font-weight: bold;
-    text-align: center;
-    margin-left: auto;
-    margin-right: auto;
-    display: flex;
-    padding-left: 20px;
-    margin: 50px;
-`
+  color: red;
+  font-size: 1.4em;
+  font-weight: bold;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  padding-left: 20px;
+  margin: 50px;
+`;
+
